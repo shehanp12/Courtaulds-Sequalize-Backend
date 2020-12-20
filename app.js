dfd38cf0c6adaf3f24b17cb5b0599c151d23edbd
shehanp12 <shehanp068@gmail.com>
2020-12-21 03:19:17 +0530

    authentication

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/',((req, res) =>{
     res.send('Hello World');
