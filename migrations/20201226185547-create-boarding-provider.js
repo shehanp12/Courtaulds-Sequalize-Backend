f575284e481c3bc9a90e116fc29f4f172a54467f
shehanp12 <shehanp068@gmail.com>
2020-12-27 01:18:10 +0530

    boardingProviders

---
+++ migrations/20201226185547-create-boarding-provider.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('boardingProviders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      contactNumber: {
+        type: Sequelize.STRING
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('boardingProviders');
+  }
+};
---
+++ models/boardingprovider.js
@@ -0,0 +1,16 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+  const boardingProvider = sequelize.define('boardingProvider',{
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    contactNumber: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    address: DataTypes.STRING
+  },{});
+
+  boardingProvider.associate = (models) =>{
+
+  }
+  return boardingProvider;
+};
