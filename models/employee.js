42fe2aa2acb1fda7a4424c56e5a870ebd2ebf3e4
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2021-01-16 22:59:04 +0530

    emp model

--- models/employee.js
+++ models/employee.js
@@ -3,7 +3,13 @@ module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('Employee',{
 
 
-    
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    password:DataTypes.STRING,
+    email:DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    employeeAdress: DataTypes.STRING,
+    departmentId: DataTypes.INTEGER
 
   },{});
 
