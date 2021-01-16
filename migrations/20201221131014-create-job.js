76616162090599349fcab6182a71ce4a3cce5650
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2021-01-16 23:01:44 +0530

    migration

--- migrations/20201221131014-create-job.js
+++ migrations/20201221131014-create-job.js
@@ -2,12 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Jobs', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
+      
       jobName: {
         type: Sequelize.STRING
       },
